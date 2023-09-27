841ce6b1e786bdeaf0f4ab3c7535a321be1c35b7
sdm <dongming.shen@sap.com>
2023-09-27 14:09:22 +0800

    update

--- app/api/model-config/route.ts
+++ app/api/model-config/route.ts
@@ -1,13 +1,16 @@
 import { NextResponse } from "next/server";
 
 async function handle() {
+  let model_list: any[] = [];
   // @ts-ignore
-  let model_list = process.env.MODEL_LIST.split(",").map((v) => {
-    return {
-      name: v,
-      available: true,
-    };
-  });
+  if (process.env.MODEL_LIST) {
+    model_list = process.env.MODEL_LIST.split(",").map((v) => {
+      return {
+        name: v,
+        available: true,
+      };
+    });
+  }
   return NextResponse.json({ model_list });
 }
 
